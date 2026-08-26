437ab86660ee1917c30294abc6aa536ed67db56c
BetawithGamma01 <lalan10mishra@betawithgamma.com>
2026-08-26 11:19:15 +0530

    v0.3: bump Android version

---
+++ ci/authfix-v03/app-build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.betawithgamma.microstructure"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.betawithgamma.microstructure"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 3
+        versionName = "0.3.0-webview-oauth"
+
+        testInstrumentationRunner = "android.test.InstrumentationTestRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    packaging {
+        resources.excludes += setOf("META-INF/DEPENDENCIES", "META-INF/LICENSE*", "META-INF/NOTICE*")
+    }
+}
+
+dependencies {
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    testImplementation("junit:junit:4.13.2")
+}
